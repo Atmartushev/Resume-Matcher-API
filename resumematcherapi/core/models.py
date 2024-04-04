273f654da2d38aad791c0b592e1c34b6ca5f9c22
HaleBradley <hbradley787@gmail.com>
2024-04-03 21:23:42 -0500

    Model changes

--- resumematcherapi/core/models.py
+++ resumematcherapi/core/models.py
@@ -2,23 +2,31 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=50)
-    pfp = models.CharField(max_length=400)
-    email = models.CharField(max_length=100)
+    username = models.CharField(max_length=250)
+    pfp = models.CharField(max_length=500)
+    email = models.CharField(max_length=300, unique=True)
 
 class Rubric(models.Model):
     sections = models.TextField()
 
 class Job(models.Model):
+    PRIORITY_CHOICES = [
+        ('low', 'Low'), 
+        ('medium', 'Medium'), 
+        ('high', 'High')
+    ]
+
     name = models.CharField(max_length=200)
-    jod_description = models.CharField(max_length=2000)
+    jod_description = models.CharField(max_length=10000)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rubric = models.ForeignKey(Rubric, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')
 
 class Candidate(models.Model):
-    name = models.CharField(max_length=50, default='Name Not Provided')
+    name = models.CharField(max_length=250, default='Name Not Provided')
     resume = models.FileField(upload_to='resumes/', default='N/A')
     resume_score = models.TextField(default='0')
     resume_score_description = models.TextField(default='N/A')
-    contact = models.CharField(max_length=50, default='Contact Not Available')
+    contact = models.CharField(max_length=100, default='Contact Not Available')
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
